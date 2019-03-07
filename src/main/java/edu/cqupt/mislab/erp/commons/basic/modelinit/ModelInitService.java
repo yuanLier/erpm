1b7a7cc418c7b007746688140f0426061d567762
chuyunfei <755708445@qq.com>
2019-03-07 19:30:50 +0800

    构建DAO持久层数据校验解决方案，以后需要存储数据校验的Repository需要继承BasicRepository就可以实现数据校验，便于数据模型校验和业务逻辑代码

--- src/main/java/edu/cqupt/mislab/erp/commons/basic/modelinit/ModelInitService.java
+++ src/main/java/edu/cqupt/mislab/erp/commons/basic/modelinit/ModelInitService.java
@@ -1,4 +1,4 @@
-package edu.cqupt.mislab.erp.commons.basic;
+package edu.cqupt.mislab.erp.commons.basic.modelinit;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
---
+++ src/main/java/edu/cqupt/mislab/erp/commons/basic/repository/BasicRepository.java
@@ -0,0 +1,24 @@
+package edu.cqupt.mislab.erp.commons.basic.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.validation.annotation.Validated;
+
+import java.io.Serializable;
+
+@NoRepositoryBean
+public interface  BasicRepository<V,K extends Serializable> extends JpaRepository<V,K> {
+
+    /*
+     * @Author: chuyunfei
+     * @Date: 2019/3/7 19:22
+     * @Description: 基本的Repository接口，项目的所有Repository需要继承这个Repository以实现数据的验证
+     **/
+
+    /*
+    1、重写save方法以达到数据校验的目的。
+    2、另外两个个方法save(Iterable<S> entities)、saveAndFlush(S entity)也是在底层调用这个方法
+     */
+    @Override
+    <S extends V> S save(@Validated S entity);
+}
