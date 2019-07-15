934db19c8ab7d96f374491e43ee700fad4630ec1
yuanLier <yuan15826770919@126.com>
2019-07-15 20:04:49 +0800

    补充周期推进时的日志信息

--- src/main/java/edu/cqupt/mislab/erp/game/compete/operation/order/advance/OrderAdvance.java
+++ src/main/java/edu/cqupt/mislab/erp/game/compete/operation/order/advance/OrderAdvance.java
@@ -31,7 +31,11 @@ public class OrderAdvance implements ModelAdvance {
     @Override
     public boolean modelHistory(EnterpriseBasicInfo enterpriseBasicInfo) {
 
+        log.info("开始记录订单模块比赛期间历史数据");
+
         // 暂时没有什么要记录的啦
+
+        log.info("订单模块-比赛期间历史数据记录成功");
 
         return true;
     }
@@ -39,6 +43,8 @@ public class OrderAdvance implements ModelAdvance {
     @Override
     @Transactional(rollbackFor = Exception.class)
     public boolean modelAdvance(EnterpriseBasicInfo enterpriseBasicInfo) {
+
+        log.info("开始进行订单模块比赛期间周期推进");
 
         // 获取全部未交货的订单
         List<GameOrderInfo> gameOrderInfoList = gameOrderInfoRepository.findByEnterpriseBasicInfo_IdAndOrderStatus(enterpriseBasicInfo.getId(), false);
@@ -56,6 +62,8 @@ public class OrderAdvance implements ModelAdvance {
                 financeService.updateFinanceInfo(enterpriseBasicInfo.getId(), changeOperating, changeAmount, true, true);
             }
         }
+
+        log.info("订单模块-比赛期间周期推进正常");
 
         return true;
     }
